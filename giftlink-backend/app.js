1635d96a0abfa14a4f0a5836af181e84a3e5c0e2
Kris Hornung <krishornung@gmail.com>
2025-12-24 11:25:24 -0500

    Completed Create API for Search Service: implemented filtered search endpoint and integrated routes in app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -25,8 +25,7 @@ app.use(express.json());
 const giftroutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-// (Leave commented until needed in later labs)
-// const searchRoutes = require('./routes/searchRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -39,8 +38,7 @@ app.use(pinoHttp({ logger }));
 app.use('/api/gifts', giftroutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-// (Leave commented until needed)
-// app.use('/api/search', searchRoutes);
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
